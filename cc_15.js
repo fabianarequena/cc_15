05e37763159a2d0dfb057a937f135dd03c9d242a
fabianarequena <fabianarequena@usf.edu>
2025-03-15 18:00:34 -0400

    Task 2 - Adding Risk Items

--- cc_15.js
+++ cc_15.js
@@ -6,3 +6,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Created basic structure with a risk dashboard container and printed text to the console
 
+// Task 2 - Adding Risk Items
+function addRiskItem(riskName, riskLevel, department) {
+    const riskCard = document.createElement("div");
+    riskCard.classList.add("riskCard");
+
+    // Test Cases for Challenge 
+    addRiskItem("Data Breach", "High", "IT");
+    addRiskItem("Supply Chain Disruption", "Medium", "Operations");
+
+// Created risk cards with the option to adjust differrnt variables; assgined classes and appended
+
